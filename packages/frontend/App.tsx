f4f1d2204ef15950addcd1c0ecd4fa6987fd34c6
Konosuke Sakai <konosukeam@gmail.com>
2025-09-28 04:24:13 +0000

    feat: integrate Physics and Suspense components within XR for enhanced VR experience

--- packages/frontend/App.tsx
+++ packages/frontend/App.tsx
@@ -1,5 +1,7 @@
 import { Canvas } from "@react-three/fiber";
+import { Physics, RigidBody } from "@react-three/rapier";
 import { createXRStore, XR } from "@react-three/xr";
+import { Suspense } from "react";
 
 const store = createXRStore({
 	offerSession: "immersive-vr",
@@ -9,7 +11,15 @@ export default function App() {
 	return (
 		<div className="w-screen h-screen">
 			<Canvas>
-				<XR store={store} />
+				<XR store={store}>
+					<Suspense>
+						<Physics>
+							<RigidBody>
+								<mesh />
+							</RigidBody>
+						</Physics>
+					</Suspense>
+				</XR>
 			</Canvas>
 		</div>
 	);
